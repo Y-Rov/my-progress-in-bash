65dde416d697e32554505ef08710e06ea71a8331
YuraRov <yura.rov555@gmail.com>
2022-08-25 12:04:25 +0300

    Finished exercise №1

---
+++ section-6/exercise-1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# This script displays one random number to the screen 
+# It also generates a syslog message with that random number 
+# with the "user" facility and the "info" severity.
+
+RANDOM_NUM=$RANDOM
+echo $RANDOM_NUM
+logger -p user.info $RANDOM_NUM
